a41e6e3c111c79e8f9d9ad5946a7b00df592306b
Ege Balcı <egebal1996@gmail.com>
2016-07-25 14:06:20 +0300

    Update Update.go

--- SOURCE/Update.go
+++ SOURCE/Update.go
@@ -13,27 +13,40 @@ import "color"
 func main() {
 
 
-	Repo := []string{"https://github.com/EgeBalci/HERCULES/blob/master/SOURCE/HERCULES.go", "https://github.com/EgeBalci/HERCULES/raw/master/HERCULES_x64", "https://github.com/EgeBalci/HERCULES/raw/master/HERCULES_x86", "https://github.com/EgeBalci/HERCULES/raw/master/README.md"}
+	Repo := [3]string{"https://github.com/EgeBalci/HERCULES/raw/master/HERCULES", "https://github.com/EgeBalci/HERCULES/raw/master/HERCULES_x64", "https://github.com/EgeBalci/HERCULES/raw/master/Update"}
 
 
 	exec.Command("sh", "-c", "zenity --info --text=\"HERCULES Update Started... \"").Run()
-	exec.Command("sh", "-c", "rm  -r SOURCE").Run()
-	exec.Command("sh", "-c", "rm  -r STATISTICS").Run()
-	exec.Command("sh", "-c", "rm HERCULES_x64").Run()
-	exec.Command("sh", "-c", "rm HERCULES_x86").Run()
+	exec.Command("sh", "-c", "sudo rm HERCULES").Run()
+	exec.Command("sh", "-c", "sudo rm HERCULES_x86").Run()
 	color.Blue("[*] Updating HERCULES...\n\n")
 	for i := 0; i < len(Repo); i++ {
 		response, _ := http.Get(Repo[i])
 		defer response.Body.Close();
-    body, _ := ioutil.ReadAll(response.Body);
+    	body, _ := ioutil.ReadAll(response.Body);
 
-    Name := strings.Split(Repo[i], "/")
-    color.Green("#	"+string(Name[(len(Name)-1)])+"		[OK]")
-    File, _ := os.Create(string(Name[(len(Name)-1)]))
-
-    File.WriteString(string(body))
+    	Name := strings.Split(Repo[i], "/")
+    	color.Green("#	"+string(Name[(len(Name)-1)])+"		[OK]")
+    	File, _ := os.Create(string(Name[(len(Name)-1)]))
+    	File.WriteString(string(body))
 	}
-	exec.Command("sh", "-c", "zenity --info --text=\"HERCULES Update completed !\"").Run()
+	Result, Status := CheckValid()
+	if Result == false {
+		color.Red(Status)
+		exec.Command("sh", "-c", "zenity --warning --text=\"HERCULES Update Failed !\"").Run()
+	}else{
+		exec.Command("sh", "-c", "zenity --info --text=\"HERCULES Update completed !\"").Run()
+	}
+	
 
 
+}
+
+
+func CheckValid()  (bool, string){
+  OutESP, _ := exec.Command("sh", "-c", "ls").Output()
+  if (!strings.Contains(string(OutESP), "HERCULES")) || (!strings.Contains(string(OutESP), "HERCULES_x64")) || (!strings.Contains(string(OutESP), "Update")) {
+    return false, "[!] ERROR : Update failed"
+  }
+  return true, ""
 }
